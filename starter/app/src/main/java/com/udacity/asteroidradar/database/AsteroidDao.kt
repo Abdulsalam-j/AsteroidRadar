514cc46f185a62bf12fa55db52c5577c68f8b09d
Abdulsalam-Aljamal <abdulsalam.aljamal@gmail.com>
2022-04-17 16:51:22 +0300

    done database

--- starter/app/src/main/java/com/udacity/asteroidradar/database/AsteroidDao.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/database/AsteroidDao.kt
@@ -1,2 +1,15 @@
 package com.udacity.asteroidradar.database
 
+import androidx.lifecycle.LiveData
+import androidx.room.Insert
+import androidx.room.Query
+
+interface AsteroidDao {
+
+    @Query("SELECT * FROM asteroids_table")
+    fun getAsteroids(): LiveData<List<AsteroidRoomDatabase>>
+
+    @Insert
+    fun insertAll(vararg asteroids: AsteroidRoomDatabase)
+
+}
--- starter/app/src/main/java/com/udacity/asteroidradar/database/DatabaseEntities.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/database/DatabaseEntities.kt
@@ -1,2 +1,30 @@
 package com.udacity.asteroidradar.database
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [AsteroidRoomDatabase::class], version = 1)
+abstract class AsteroidsDatabase: RoomDatabase() {
+
+    abstract val asteroidDao: AsteroidDao
+
+    companion object {
+        @Volatile
+        private lateinit var INSTANCE: AsteroidsDatabase
+
+        fun getDatabase(context: Context): AsteroidsDatabase {
+            synchronized(AsteroidsDatabase::class.java) {
+                if (!::INSTANCE.isInitialized) {
+                    INSTANCE = Room.databaseBuilder(
+                        context.applicationContext,
+                        AsteroidsDatabase::class.java,
+                        "AsteroidsDatabase"
+                    ).build()
+                }
+            }
+            return INSTANCE
+        }
+    }
+}
