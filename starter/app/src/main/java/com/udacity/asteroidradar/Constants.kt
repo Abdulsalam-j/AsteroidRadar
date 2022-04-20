0e9f3852bddd93973be7fdf9b17ebcc5f011d394
Abdulsalam-Aljamal <abdulsalam.aljamal@gmail.com>
2022-04-21 00:28:05 +0300

    hide API Key

--- starter/app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
     const val API_QUERY_DATE_FORMAT = "yyyy-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    const val API_KEY= "9mUwdUNutEe3e6r6gg7ZBJCJGLUOtGmt6ctxohaQ"
+    const val API_KEY= "YOUR_API_KEY"
     const val DEFAULT_IMAGE_URL = "https://apod.nasa.gov/apod/image/2204/N11_HubbleLake_1600.jpg"
     const val DEFAULT_IMAGE_TITLE = "Star Clouds of the LMC"
 }
